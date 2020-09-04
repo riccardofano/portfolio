82527de4477fb4cbc34dda1a776d7fdafe6ff2f1
riccardofano <rcr.fano@gmail.com>
2020-09-04 16:23:17 +0200

    cleanup index page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
+  <div class="root">
     <Nuxt />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.root {
+  display: grid;
+  grid-template-columns: minmax(0, 2rem) 1fr minmax(0, 2rem);
+
+  & > * {
+    grid-column: 2;
+  }
+}
+</style>
