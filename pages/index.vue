7ba5605b990de4d5eb9fdd26db7e71485cece6f6
riccardofano <rcr.fano@gmail.com>
2020-09-04 17:06:32 +0200

    add style to hero

--- pages/index.vue
+++ pages/index.vue
@@ -15,4 +15,25 @@
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.hero {
+  text-align: center;
+
+  &__title {
+    font-size: 2rem;
+    font-weight: 600;
+    line-height: 1.2;
+    margin: 3rem 0;
+  }
+
+  &__subtitle {
+    font-size: 1.25rem;
+    display: block;
+    font-weight: 400;
+  }
+
+  &__links {
+    margin-bottom: 4rem;
+  }
+}
+</style>
