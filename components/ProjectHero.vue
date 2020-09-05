6d8d983017273ca412c43fc5d065257e22e59bc4
riccardofano <rcr.fano@gmail.com>
2020-09-05 18:23:24 +0200

    add project page with hero

---
+++ components/ProjectHero.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="hero grid grid--outer">
+    <style>
+      :root {
+        --hero-image: url('{{imageUrl}}');
+      }
+    </style>
+    <h1 class="hero__title">
+      <span class="underline">{{ firstLine }}</span>
+      <span class="underline">{{ secondLine }}</span>
+    </h1>
+    <div class="hero__links">
+      <a class="btn" :href="repoUrl">Github</a>
+      <a class="btn btn--highlight" :href="demoUrl">Versione live</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectHero',
+  props: {
+    firstLine: {
+      type: String,
+      required: true,
+    },
+    secondLine: {
+      type: String,
+      required: true,
+    },
+    repoUrl: {
+      type: String,
+      required: true,
+    },
+    demoUrl: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  text-align: center;
+  grid-column: 1 / -1;
+  background-image: var(--hero-image);
+  z-index: -2;
+
+  &__title {
+    margin: 3rem 0;
+    font-size: 2rem;
+    font-weight: 600;
+    line-height: 1.2;
+  }
+
+  &__links {
+    margin-bottom: 4rem;
+
+    & a {
+      text-decoration: none;
+      color: var(--clr-text);
+    }
+  }
+}
+</style>
---
+++ pages/project.vue
@@ -0,0 +1,19 @@
+<template>
+  <project-hero
+    first-line="Piante"
+    second-line="e-commerce"
+    repo-url="#"
+    demo-url="#"
+    image-url="https://via.placeholder.com/400x300"
+  ></project-hero>
+</template>
+
+<script>
+import ProjectHero from '@/components/ProjectHero.vue'
+
+export default {
+  components: { ProjectHero },
+}
+</script>
+
+<style></style>
