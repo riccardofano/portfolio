d1abb34ddaa672b4c3575b190d48817d5d093799
riccardofano <rcr.fano@gmail.com>
2020-09-05 10:06:02 +0200

    add the-hero component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,5 @@
 <template>
-  <div class="root">
+  <div class="grid grid--outer">
     <Nuxt />
   </div>
 </template>
-
-<style lang="scss" scoped>
-.root {
-  display: grid;
-  grid-template-columns: minmax(0, 2rem) 1fr minmax(0, 2rem);
-
-  & > * {
-    grid-column: 2;
-  }
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,15 @@
 <template>
-  <div class="hero">
-    <div class="hero__heading">
-      <h2 class="hero__subtitle">Sono Riccardo Fano</h2>
-      <h1 class="hero__title">
-        <span class="underline"> Web Developer e</span>
-        <span class="underline">Designer</span>
-      </h1>
-    </div>
-    <div class="hero__links">
-      <a class="btn">Scarica CV</a>
-      <a class="btn btn--highlight">Contattami</a>
-    </div>
-  </div>
+  <fragment>
+    <the-hero></the-hero>
+  </fragment>
 </template>
 
 <script>
-export default {}
+import TheHero from '@/components/TheHero.vue'
+
+export default {
+  components: { TheHero },
+}
 </script>
 
-<style lang="scss" scoped>
-.hero {
-  text-align: center;
-
-  &__heading {
-    margin: 3rem 0;
-  }
-
-  &__title {
-    font-size: 2rem;
-    font-weight: 600;
-    line-height: 1.2;
-  }
-
-  &__subtitle {
-    font-size: 1.25rem;
-    display: block;
-    font-weight: 400;
-  }
-
-  &__links {
-    margin-bottom: 4rem;
-  }
-}
-</style>
+<style></style>
