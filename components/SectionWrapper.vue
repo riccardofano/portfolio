1556a072fc58f030c3c1d8221203fd57831a3daa
riccardofano <rcr.fano@gmail.com>
2020-09-05 10:40:12 +0200

    add section-wrapper component

---
+++ components/SectionWrapper.vue
@@ -0,0 +1,39 @@
+<template>
+  <section v-if="background" class="section section--background">
+    <div class="grid grid--outer">
+      <h2 class="section__header underline">{{ header }}</h2>
+      <slot></slot>
+    </div>
+  </section>
+  <section v-else class="section">
+    <h2 class="section__header underline">{{ header }}</h2>
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SectionWrapper',
+  props: {
+    background: { type: Boolean, required: false },
+    header: { type: String, required: true },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.section {
+  padding: 3rem 0;
+  grid-column: 2;
+
+  &--background {
+    grid-column: 1 / -1;
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+
+  &__header {
+    display: inline;
+    font-size: 1.5rem;
+  }
+}
+</style>
