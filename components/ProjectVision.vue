db6b59a3ee91a48627346346beb118335ef9badd
riccardofano <rcr.fano@gmail.com>
2020-09-06 12:49:44 +0200

    add project vision to project page

---
+++ components/ProjectVision.vue
@@ -0,0 +1,29 @@
+<template>
+  <fragment>
+    <section-wrapper>
+      <div class="grid grid--inner">
+        <div class="vision">
+          <h2 class="header underline">Visione del progetto</h2>
+          <p class="vision__description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+            numquam. Beatae omnis dolor assumenda est repellat voluptatem, quasi
+            vel amet ratione nulla quisquam fuga perspiciatis molestias
+            asperiores fugiat eos cum minus quam culpa, libero sequi consequatur
+            necessitatibus rerum. Dolorem, cupiditate?
+          </p>
+        </div>
+      </div>
+    </section-wrapper>
+  </fragment>
+</template>
+
+<script>
+import SectionWrapper from '@/components/SectionWrapper.vue'
+
+export default {
+  name: 'ProjectVision',
+  components: { SectionWrapper },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/project.vue
+++ pages/project.vue
@@ -1,18 +1,22 @@
 <template>
-  <project-hero
-    first-line="Piante"
-    second-line="e-commerce"
-    repo-url="#"
-    demo-url="#"
-    image-url="https://via.placeholder.com/400x300"
-  ></project-hero>
+  <fragment>
+    <project-hero
+      first-line="Piante"
+      second-line="e-commerce"
+      repo-url="#"
+      demo-url="#"
+      image-url="https://via.placeholder.com/400x300"
+    ></project-hero>
+    <project-vision></project-vision>
+  </fragment>
 </template>
 
 <script>
 import ProjectHero from '@/components/ProjectHero.vue'
+import ProjectVision from '@/components/ProjectVision.vue'
 
 export default {
-  components: { ProjectHero },
+  components: { ProjectHero, ProjectVision },
 }
 </script>
 
