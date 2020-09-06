e4aa584a74eddb284f6888954e24e3cfebe7c797
riccardofano <rcr.fano@gmail.com>
2020-09-06 22:39:23 +0200

    make about section responsive

--- components/SectionAbout.vue
+++ components/SectionAbout.vue
@@ -1,25 +1,29 @@
 <template>
   <section-wrapper>
     <h2 class="header underline">Più su di me</h2>
-      <p class="about__description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste laborum
-        expedita quam! Veritatis blanditiis ex ipsa vel delectus, officia magni?
-      </p>
+    <div class="about grid grid--inner">
+      <div class="about__info">
+        <p class="about__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste laborum
+          expedita quam! Veritatis blanditiis ex ipsa vel delectus, officia
+          magni?
+        </p>
+        <h3 class="about__header">Strumenti e abilità</h3>
+        <p class="about__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ipsa,
+          ullam unde fugiat quam corrupti maiores fugit iure illum dignissimos
+          architecto repudiandae. Fugit culpa alias, id adipisci quisquam
+          expedita! Amet.
+        </p>
+        <p class="about__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+          neque sequi enim, quaerat est veniam tempore dolore et ad eum quas
+          provident repellat dolorem eveniet?
+        </p>
+      </div>
       <picture class="about__picture picture">
         <img src="https://via.placeholder.com/400x300" alt="" />
       </picture>
-      <h3 class="about__header">Strumenti e abilità</h3>
-      <p class="about__description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ipsa,
-        ullam unde fugiat quam corrupti maiores fugit iure illum dignissimos
-        architecto repudiandae. Fugit culpa alias, id adipisci quisquam
-        expedita! Amet.
-      </p>
-      <p class="about__description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, neque
-        sequi enim, quaerat est veniam tempore dolore et ad eum quas provident
-        repellat dolorem eveniet?
-      </p>
     </div>
   </section-wrapper>
 </template>
@@ -35,12 +39,21 @@ export default {
 
 <style lang="scss" scoped>
 .about {
+  &__picture {
+    justify-self: center;
+  }
   &__picture img {
-    margin: 1.5rem 0;
+    margin-top: 1.5rem;
+    box-shadow: none;
   }
 
   &__header {
-    margin-bottom: 0.75rem;
+    margin: 1rem 0 0.75rem;
+    font-size: 1.1rem;
+
+    @include for-tablet-up {
+      font-size: 1.25rem;
+    }
   }
 
   &__description + &__description {
